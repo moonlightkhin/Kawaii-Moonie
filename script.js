a18f0740ad84ee5bff1dfd593b177c19f105a33e
moonlightkhin <125897584+moonlightkhin@users.noreply.github.com>
2026-03-09 10:22:17 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-// Starting positions
 let hatIndex = 1;
 let dressIndex = 1;
 let shoesIndex = 1;
 
-// Hat Functions
 function nextHat() {
-    hatIndex++;
-    if (hatIndex > 9) hatIndex = 1;
+    hatIndex = (hatIndex >= 9) ? 1 : hatIndex + 1;
     document.getElementById("hat").src = "hat" + hatIndex + ".png";
 }
 function prevHat() {
-    hatIndex--;
-    if (hatIndex < 1) hatIndex = 9;
+    hatIndex = (hatIndex <= 1) ? 9 : hatIndex - 1;
     document.getElementById("hat").src = "hat" + hatIndex + ".png";
 }
 
-// Dress Functions
 function nextDress() {
-    dressIndex++;
-    if (dressIndex > 9) dressIndex = 1;
+    dressIndex = (dressIndex >= 9) ? 1 : dressIndex + 1;
     document.getElementById("dress").src = "dress" + dressIndex + ".png";
 }
 function prevDress() {
-    dressIndex--;
-    if (dressIndex < 1) dressIndex = 9;
+    dressIndex = (dressIndex <= 1) ? 9 : dressIndex - 1;
     document.getElementById("dress").src = "dress" + dressIndex + ".png";
 }
 
-// Shoes Functions
 function nextShoes() {
-    shoesIndex++;
-    if (shoesIndex > 4) shoesIndex = 1;
+    shoesIndex = (shoesIndex >= 4) ? 1 : shoesIndex + 1;
     document.getElementById("shoes").src = "shoes" + shoesIndex + ".png";
 }
 function prevShoes() {
-    shoesIndex--;
-    if (shoesIndex < 1) shoesIndex = 4;
+    shoesIndex = (shoesIndex <= 1) ? 4 : shoesIndex - 1;
     document.getElementById("shoes").src = "shoes" + shoesIndex + ".png";
 }
